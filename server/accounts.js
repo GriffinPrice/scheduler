adcfdaf2a77bbe2eefb7d91d67ce462029f04968
Griffin Price <griffin.price@gmail.com>
2014-11-22 17:17:06 -0800

    First implementation of JWT auth and calendar list requests

--- server/accounts.js
+++ server/accounts.js
@@ -5,54 +5,28 @@ ServiceConfiguration.configurations.insert({
   secret: "6MQ8L93gbu9xMUhPydHC7OlR"
 });
 
-/* THIS CODE WAS A PREVIOUS START TO IMPLEMENTING JWT BEFORE I FOUND A PACKAGE FOR IT
-function getGoogleAccessToken(user){
-
-    //Standard Google Header. RSA SHA-256 algorithm and JWT token format
-    var googleJWTHeader = {"alg":"RS256","typ":"JWT"}
-
-    //var base64header= JWTHeader();
-    var d = new Date;
-    var currentTimeMS = d.getUTCMilliseconds;
-    var expyMinutes = 15;                                   //We are creating an access token that will last for 15 minutes.
-    var expTimeMS = currentTime + (expyMinutes * 60 * 1000)                         //I don't know the benefits/drawbacks of using the max time of 1hour.
-    var claims = {
-        "iss":"732547433082-lbs4a1ktib49b34muh9bg2bjm93791lo@developer.gserviceaccount.com", //Application google service account e-mail
-        "scope": "https://www.googleapis.com/auth/calendar",                    //Application requests access to user calendar
-        "aud": "https://accounts.google.com/o/oauth2/token",                                 //We're requesting an authentication token.
-        "exp": expTimeMS,
-        "iat": currentTimeMS
-    }
-    //var base64claims= JWTClaimset(claims);
-
-    var base64signature = JWTSignature(user.profile.services.google.accessToken);
-
-    var JWTrequest = base64header + "." + base64claims + "." + base64signature;
+Meteor.startup(function(){
+    HTTPJWT.setJWTOptions({ // Just call this once to set JWT
+      email: '<google_service_account_email_local_part>@developer.gserviceaccount.com',
+      key: Assets.getText('key.pem'), // Get key file from assets
+      scopes: [
+        'https://www.googleapis.com/auth/calendar // New scope name
+      ]
+    });
 }
-
-
-//Wrapper to encode the google JWT header. In the future, this may need to be mroe robust.
-function JWTHeader(){
-    return Meteor.call('base64UrlEncode',googleJWTHeader);
-}
-
-//Wrapper to encode claims. It is separate from encoding the header because I believe I may change the structure later.
-function JWTClaimset(claims){
-    return Meteor.call('base64UrlEncode',claims);
-}
-
-
-//Generates the signature from the users accessToken.
-function JWTSignature(accessToken){
-
-}
-*/
-
 
 function getCalendarList(user){
-    var gToken= user.profile.googleToken;
+    var userID = user.profile.services.google.id;
     // Need to authenticate with Google first by sending an OAuth2 authentication request.
-    //To this end,
+    var APIurl = 'https://www.googleapis.com/calendar/v3/users/' + userID + '/calendarList';
+    var result = HTTPJWT.get(APIurl);
+    var calArray = new Array();
+    _.each(result.item,function(calendar){
+        calArray.push(calendar.id);
+    });
+    user.profile.calendars = calArray;
+
+
 }
 
 /* valudateNewUser hook calls function every time it runs, and creates starting user profile info.
